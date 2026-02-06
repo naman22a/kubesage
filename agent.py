5804a801232c7f2c7f3ed6d625f7462dd504934a
Naman Arora <namanarora1022@gmail.com>
2026-02-06 13:35:12 +0530

    feat: add .env

--- agent.py
+++ agent.py
@@ -2,6 +2,8 @@ import io
 from contextlib import redirect_stdout
 from pprint import pprint
 import argparse
+import os
+import dotenv
 
 from strands import Agent
 from strands.models.ollama import OllamaModel
@@ -10,6 +12,8 @@ from k8s import list_pod_with_logs
 from constants import system_prompt
 from custom_types import K8sAgentResult
 from fns import build_agent_context
+
+dotenv.load_dotenv()
 
 parser = argparse.ArgumentParser(
     prog='KubeSage',
@@ -29,7 +33,7 @@ else:
 
 ollama_model = OllamaModel(
     host="http://localhost:11434",
-    model_id="qwen3-coder"
+    model_id=os.environ['MODEL_ID']
 )
 
 agent = Agent(
